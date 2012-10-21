f04395a2715daeccf7e7e84dda4880f9a3ee1294
Torben Nielsen <github@torbennielsen.org>
2012-10-21 12:47:03 +0200

    Print playlist contents

--- spotifeed.php
+++ spotifeed.php
@@ -1,2 +1,2 @@
 $the_playlist = "https://embed.spotify.com/?uri=spotify:user:illztherocker:playlist:4hYlcIhuqsKl6VxN59POX9"
-echo file_get_contents('http://www.google.com');
+echo file_get_contents($the_playlist);
